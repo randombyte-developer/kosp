e65bf6fa04ccb75b60b0a7cc823e5ab2ef9812f0
randombyte-developer <apps.randombyte@gmail.com>
2016-10-03 18:30:59 +0200

    Use own extensions ;)

--- src/main/kotlin/de/randombyte/kosp/PlayerExecutedCommand.kt
+++ src/main/kotlin/de/randombyte/kosp/PlayerExecutedCommand.kt
@@ -1,13 +1,12 @@
 package de.randombyte.kosp
 
+import de.randombyte.kosp.StringExtensions.red
 import org.spongepowered.api.command.CommandException
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
 import org.spongepowered.api.command.spec.CommandExecutor
 import org.spongepowered.api.entity.living.player.Player
-import org.spongepowered.api.text.Text
-import org.spongepowered.api.text.format.TextColors
 
 /**
  * A command that can only be executed by a [Player]. Otherwise it will throw a [CommandException].
@@ -16,7 +15,7 @@ abstract class PlayerExecutedCommand : CommandExecutor {
     abstract fun executedByPlayer(player: Player, args: CommandContext): CommandResult
 
     override fun execute(src: CommandSource, args: CommandContext): CommandResult {
-        if (src !is Player) throw CommandException(Text.of(TextColors.RED, "Command must be executed by a player!"))
+        if (src !is Player) throw CommandException("Command must be executed by a player!".red())
         return executedByPlayer(src, args)
     }
 }
