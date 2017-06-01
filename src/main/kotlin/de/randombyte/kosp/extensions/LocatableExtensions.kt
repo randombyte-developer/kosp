d1dda9cb61449a39f8cf11e4a575bc7c76e7adbb
randombyte-developer <apps.randombyte@gmail.com>
2017-06-01 13:25:49 +0200

    added getNearest() extension; new version

---
+++ src/main/kotlin/de/randombyte/kosp/extensions/LocatableExtensions.kt
@@ -0,0 +1,11 @@
+package de.randombyte.kosp.extensions
+
+import org.spongepowered.api.world.Locatable
+import org.spongepowered.api.world.Location
+import org.spongepowered.api.world.World
+
+fun <T : Locatable> Iterable<T>.getNearest(location: Location<World>): T? = filter {
+    it.location.extent.uniqueId == location.extent.uniqueId
+}.minBy {
+    it.location.position.distance(location.position)
+}
