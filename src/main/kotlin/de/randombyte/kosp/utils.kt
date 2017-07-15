a95eb550f7412edbe0f46a74c6c9e13e9397cdf4
randombyte-developer <apps.randombyte@gmail.com>
2017-07-15 21:57:12 +0200

    updated kotlin; new version

--- src/main/kotlin/de/randombyte/kosp/utils.kt
+++ src/main/kotlin/de/randombyte/kosp/utils.kt
@@ -1,7 +1,9 @@
 package de.randombyte.kosp
 
+import de.randombyte.kosp.extensions.executeCommand
 import org.spongepowered.api.Sponge
 import org.spongepowered.api.profile.GameProfile
+import org.spongepowered.api.service.economy.EconomyService
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.TextTemplate
 import java.util.concurrent.CompletableFuture
@@ -12,12 +14,13 @@ import kotlin.reflect.KClass
  */
 
 fun <T : Any> getServiceOrFail(clazz: KClass<T>, failMessage: String = "'${clazz.java.simpleName}' is not available!"): T {
-    return Sponge.getServiceManager().provide(clazz.java).orElseThrow { RuntimeException(failMessage) }
+    val message = if (clazz == EconomyService::class) "No economy plugin is available!" else failMessage
+    return Sponge.getServiceManager().provide(clazz.java).orElseThrow { RuntimeException(message) }
 }
 
 fun getGameProfile(playerName: String): CompletableFuture<GameProfile> = Sponge.getServer().gameProfileManager.get(playerName)
 
-fun executeAsConsole(command: String) = Sponge.getCommandManager().process(Sponge.getServer().console, command)
+fun executeAsConsole(command: String) = Sponge.getServer().console.executeCommand(command)
 
 /**
  * Use this instead of TextTemplate.of(...) to workaround [this issue](https://github.com/SpongePowered/SpongeCommon/issues/1152).
