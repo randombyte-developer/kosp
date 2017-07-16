db6e08a9c82ce327e884691cd8c1c5a05923381d
randombyte-developer <apps.randombyte@gmail.com>
2017-07-16 17:04:42 +0200

    made SimpleDateTypeSerializer more accessible; new version

--- src/main/kotlin/de/randombyte/kosp/config/serializers/date/SimpleDateTypeSerializer.kt
+++ src/main/kotlin/de/randombyte/kosp/config/serializers/date/SimpleDateTypeSerializer.kt
@@ -12,17 +12,17 @@ object SimpleDateTypeSerializer : TypeSerializer<Date> {
 
     private val dateFormat = SimpleDateFormat("HH:mm:ss.SSS dd.MM.yyyy")
 
-    override fun deserialize(type: TypeToken<*>, value: ConfigurationNode): Date {
-        val string = value.string
-        try {
-            return dateFormat.parse(string)
-        } catch (exception: ParseException) {
-            throw ObjectMappingException("Invalid input value '$string' for a date like this: '21:18:25.300 28.03.2017'", exception)
-        }
-
-    }
+    override fun deserialize(type: TypeToken<*>, value: ConfigurationNode): Date = deserialize(value.string)
 
     override fun serialize(type: TypeToken<*>, date: Date, value: ConfigurationNode) {
-        value.value = dateFormat.format(date)
+        value.value = serialize(date)
     }
+
+    fun deserialize(string: String): Date = try {
+        dateFormat.parse(string)
+    } catch (exception: ParseException) {
+        throw ObjectMappingException("Invalid input value '$string' for a date like this: '21:18:25.300 28.03.2017'", exception)
+    }
+
+    fun serialize(date: Date): String = dateFormat.format(date)
 }
