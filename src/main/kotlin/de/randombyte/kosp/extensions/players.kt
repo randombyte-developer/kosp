d17b9bca9136347478242f1ea815764987fe215b
randombyte-developer <apps.randombyte@gmail.com>
2018-05-06 17:38:20 +0200

    Prevent items getting into equipment slots; new version

--- src/main/kotlin/de/randombyte/kosp/extensions/players.kt
+++ src/main/kotlin/de/randombyte/kosp/extensions/players.kt
@@ -5,7 +5,10 @@ import org.spongepowered.api.data.key.Keys
 import org.spongepowered.api.data.type.HandTypes
 import org.spongepowered.api.entity.EntityTypes
 import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.api.item.inventory.Inventory
 import org.spongepowered.api.item.inventory.ItemStack
+import org.spongepowered.api.item.inventory.entity.MainPlayerInventory
+import org.spongepowered.api.item.inventory.query.QueryOperationTypes
 import org.spongepowered.api.item.inventory.transaction.InventoryTransactionResult
 
 /**
@@ -20,10 +23,12 @@ fun Player.give(itemStack: ItemStack) {
         // nothing in hand -> put item in hand
         setItemInHand(HandTypes.MAIN_HAND, itemStack)
     } else {
-        // something in hand -> place item somewhere in inventory
-        val transactionResult = inventory.offer(itemStack)
+        // something in hand -> place item somewhere in the main player inventory(not equipment slots)
+        val transactionResult = inventory
+                .query<Inventory>(QueryOperationTypes.INVENTORY_TYPE.of(MainPlayerInventory::class.java))
+                .offer(itemStack)
         if (transactionResult.type != InventoryTransactionResult.Type.SUCCESS) {
-            // inventory full -> spawn as item
+            // main inventory full -> spawn as item
             val entity = location.extent.createEntity(EntityTypes.ITEM, location.position)
             entity.offer(Keys.REPRESENTED_ITEM, itemStack.createSnapshot())
             if (!location.extent.spawnEntity(entity)) {
